0fd46c324b9f175d3b22afef4d6177e84bd20a7d
Student <example@example.com>
2020-02-17 10:54:21 -0800

    add state to project.:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
 import { Switch, Route } from 'react-router-dom';
-import NewTicketForm from './NewTicketForm';
+import NewTicketControl from './NewTicketControl';
 
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketForm} />
-      </Switch>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList: []
+    };
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    //needed this
+  }
+  
+handledAddingNewTicketToList(newTicket){
+  var newMasterTicketList = this.state.masterTicketList.slice();
+  newMasterTicketList.push(newTicket);
+  this.setState({masterTicketList: newMasterTicketList});
 }
 
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />        
+        </Switch>
+      </div>
+    );
+  }
+}
 export default App;
